ba3e5e55c3e7c8e1c3ad24cc13d5e009ca9f43f7
anselmusmakai143839-ai <anselmusmakai.143839@marwadiuniversity.ac.in>
2026-07-23 12:50:35 +0530

    Add files via upload

---
+++ last. program.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+void main()
+{
+    int a[3][3],b[3][3],c[3][3],i,j,k;
+
+    printf("\n Enter Value for Matrix A\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\n Enter Value [%d] : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+     printf("\n Enter Value for Matrix B\n");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             printf("\n Enter Value [%d][%d] : ",i,j);
+             scanf("%d",&b[i][j]);
+         }
+     }
+     //logic
+     printf("\n Matrix A\n");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             printf("%5d",a[i][j]);
+         }
+         printf("\n");
+    }
+    printf("\n Matrix c\n");
+    for(i=0;i<3;i++)
+    {
+        for (j = 0; j <3; j++)
+            printf("%5d", c[i][j]);
+        }
+        printf("\n");
+        {
+
+    }
+
+}
+
+
+
+
+
